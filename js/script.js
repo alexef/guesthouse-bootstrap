086dc8af67047520a7dc367af32abc8493dd7d3e
Mio <mio930@gmail.com>
2014-08-31 20:41:54 +0800

    working on the fade

--- js/script.js
+++ js/script.js
@@ -7,17 +7,53 @@ $(document).ready(function(){
     }
 
     Slideshow.prototype = {
+        constructor: Slideshow,
         startShow: function () {
-            console.log("hello i just got called");
+            var parent = this;
             setInterval(function () {
-                if (this.currentIndex === this.pictures.length - 1) {
-                    $(this.element).css("background-image", this.pictures[0]);
-                    this.currentIndex = 0;
-                } else if (this.currentIndex < this.pictures.length - 1) {
-                    $(this.element).css("background-image", this.pictures[this.currentIndex + 1]);
-                    this.currentIndex += 1;
+                if (parent.currentIndex === parent.pictures.length - 1) {
+                    $(parent.element).fadeTo('slow', 0.2, function () {
+                        $(parent.element).fadeTo('slow', 1, function () {
+                            $(parent.element).css("background-image", parent.pictures[0]);
+                            parent.currentIndex = 0;
+                        });
+
+                    });
+                } else if (parent.currentIndex < parent.pictures.length - 1) {
+                    $(parent.element).fadeTo('slow', 0.2, function () {
+                        $(parent.element).fadeTo('slow',1, function () {
+                            $(parent.element).css("background-image", parent.pictures[parent.currentIndex + 1]);
+                            parent.currentIndex += 1;
+                        });
+                    });
                 }
             }, this.timeInterval);
+        },
+
+        clickLeft: function(){
+            var parent = this;
+            $(parent.element.img).click(function(){
+                if (parent.currentIndex === 0) {
+                    $(parent.element).css("background-image",parent.pictures[parent.pictures.length - 1]);
+                    parent.currentIndex = parent.pictures.length - 1;
+                } else if (parent.currentIndex > 0) {
+                    $(parent.element).css("background-image",parent.pictures[parent.currentIndex - 1]);
+                    parent.currentIndex -= 1;
+                }
+            });
+        },
+
+        clickRight: function(){
+            var parent = this;
+            $(parent.element.img).click(function(){
+                if (parent.currentIndex === parent.pictures.length - 1) {
+                    $(parent.element).css("background-image",parent.pictures[parent.pictures.length - 1]);
+                    parent.currentIndex = parent.pictures.length - 1;
+                } else if (parent.currentIndex > 0) {
+                    $(parent.element).css("background-image",parent.pictures[parent.currentIndex - 1]);
+                    parent.currentIndex -= 1;
+                }
+            });
         }
     };
 
@@ -28,5 +64,6 @@ $(document).ready(function(){
     var time = 4000;
     var x = new Slideshow(pictureLinks, currentIndex, element, time);
     x.startShow();
+    x.clickLeft();
 
 });
