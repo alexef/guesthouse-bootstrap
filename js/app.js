f566929135b97cacaf4fadc2aa8c718d140671bd
Alex Eftimie <alex@eftimie.ro>
2014-09-13 11:17:07 +0300

    merged

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-//require('js/vendor/handlebars.runtime');
 require('js/vendor/director.min.js');
 require('js/vendor/jquery-2.1.1.min');
+require('js/vendor/handlebars.runtime-v1.3.0');
 require('build/templates');
 require('js/templates');
 require('js/slideshow');
